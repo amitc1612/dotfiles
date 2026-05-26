0ead271cd95e276c805d05eff81969a8d3bf5662
Amit Cohen <59233615+amitc1612@users.noreply.github.com>
2026-05-26 22:29:49 +0000

    created bare git repository on host

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,13 +1,8 @@
 alias zshrc='$EDITOR $ZDOTDIR/.zshrc'
 alias zshenv='$EDITOR $HOME/.zshenv'
 alias aliases='$EDITOR $ZDOTDIR/aliases.zsh'
-alias r1="ollama run deepseek-r1:1.5b"
-alias g3="ollama run gemma3:1b"
-alias has="curl -sL https://git.io/_has | bash -s"
-alias plex="sudo docker-compose -f ~/media_server/docker-compose.yaml"
 alias cat="bat"
 alias mc="micro"
 alias nano="micro"
 alias q="exit"
 alias dot='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias doth='git --git-dir=$HOME/.config/hypr/.git  --work-tree=$HOME/.config/hypr'
